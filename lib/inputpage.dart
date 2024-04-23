43828506c9929425002bd7bebc60fd7e7fdd7956
moha_kh_23 <khrijimohamedahmed@gmail.com>
2024-04-23 15:24:57 +0200

    commit 5

--- lib/inputpage.dart
+++ lib/inputpage.dart
@@ -3,18 +3,45 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'card.dart';
 import 'icon.dart';
 
-const bottomContainerHeight = 80.0;
-const activeCardColour = Color(0xFF1D1E33);
-const bottomContainerColor = Colors.red;
+const double bottomContainerHeight = 80.0;
+const Color activeCardColour = Color(0xFF1D1E33);
+const Color inactiveCardColour = Color(0xFF111328);
+const Color bottomContainerColor = Color(0xFFEB1555);
 
-class InputPage extends StatelessWidget {
-  const InputPage({Key? key}) : super(key: key);
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Color maleCardColour = inactiveCardColour;
+  Color femaleCardColour = inactiveCardColour;
+
+  void updateColour(int gender) {
+    setState(() {
+      if (gender == 1) {
+        if (maleCardColour == inactiveCardColour) {
+          maleCardColour = activeCardColour;
+          femaleCardColour = inactiveCardColour;
+        } else {
+          maleCardColour = inactiveCardColour;
+        }
+      } else {
+        if (femaleCardColour == inactiveCardColour) {
+          femaleCardColour = activeCardColour;
+          maleCardColour = inactiveCardColour;
+        } else {
+          femaleCardColour = inactiveCardColour;
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BMI CALCULATOR'),
+        title: Text('BMI CALCULATOR'),
       ),
       body: Column(
         children: [
@@ -22,20 +49,30 @@ class InputPage extends StatelessWidget {
             child: Row(
               children: [
                 Expanded(
-                  child: CardClass(
-                    colour: activeCardColour,
-                    cardChild: Iconcard(
-                      icon: FontAwesomeIcons.mars,
-                      label: 'MALE',
+                  child: GestureDetector(
+                    onTap: () {
+                      updateColour(1);
+                    },
+                    child: CardClass(
+                      colour: maleCardColour,
+                      cardChild: Iconcard(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
                     ),
                   ),
                 ),
                 Expanded(
-                  child: CardClass(
-                    colour: activeCardColour,
-                    cardChild: Iconcard(
-                      icon: FontAwesomeIcons.venus,
-                      label: 'FEMALE',
+                  child: GestureDetector(
+                    onTap: () {
+                      updateColour(2);
+                    },
+                    child: CardClass(
+                      colour: femaleCardColour,
+                      cardChild: Iconcard(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
                     ),
                   ),
                 ),
@@ -44,7 +81,7 @@ class InputPage extends StatelessWidget {
           ),
           Expanded(
             child: CardClass(
-              colour: activeCardColour,
+              colour: inactiveCardColour,
             ),
           ),
           Expanded(
@@ -52,12 +89,12 @@ class InputPage extends StatelessWidget {
               children: [
                 Expanded(
                   child: CardClass(
-                    colour: activeCardColour,
+                    colour: inactiveCardColour,
                   ),
                 ),
                 Expanded(
                   child: CardClass(
-                    colour: activeCardColour,
+                    colour: inactiveCardColour,
                   ),
                 ),
               ],
